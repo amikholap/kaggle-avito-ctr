4952f24d08ab20457f59436823c9a5d5fed34d1a
amikholap <ales.mikholap@gmail.com>
2015-07-22 12:54:39 +0300

    Faster but riskier DB updates

--- run_container.sh
+++ run_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker stop kaggle-avito-ctr
 sudo docker rm kaggle-avito-ctr
-sudo docker run -d --name kaggle-avito-ctr -e TERM=linux -e PGOPTIONS="-c work_mem=128MB" -v /home/$USER/.bashrc:/root/.bashrc:ro -v $PWD/docker/var/lib/postgresql/data:/var/lib/postgresql/data -v $PWD/data:/avito/data -v $PWD/scripts:/avito/scripts -p 5432:5432 postgres
+sudo docker run -d --name kaggle-avito-ctr -e TERM=linux -v /home/$USER/.bashrc:/root/.bashrc:ro -v $PWD/docker/var/lib/postgresql/data:/var/lib/postgresql/data -v $PWD/data:/avito/data -v $PWD/scripts:/avito/scripts -p 5432:5432 postgres postgres -c checkpoint_segments=300 -c fsync=off -c full_page_writes=off -c work_mem=128MB
